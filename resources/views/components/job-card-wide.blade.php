2495ea9d382bcf415f45ced269e962f9f3d62eef
mrmatteorusso <mrmatteorusso:gmail.com>
2024-10-06 11:25:42 +0100

    first commit

---
+++ resources/views/components/job-card-wide.blade.php
@@ -0,0 +1,29 @@
+<x-panel class="flex gap-x-6">
+
+    <div>
+        <x-employer-logo></x-employer-logo>
+    </div>
+    <div class="flex-1 flex flex-col">
+        <a href="#" class="self-start text-sm text-gray-400">Laracasts</a>
+        <h3 class="font-bold text-xl mt-3 group-hover:text-blue-800 transition-colors duration-300">Video Producer</h3>
+        <p class="text-sm text-gray-400 mt-auto">Full Time - From 60,000 USD</p>
+    </div>
+
+
+
+
+    <div class="">
+        <div>
+            <p>Remote</p>
+            <p>22h</p>
+        </div>
+
+        <div>
+            <x-tag>asd</x-tag>
+            <x-tag>gfdg</x-tag>
+            <x-tag>dweetg</x-tag>
+
+        </div>
+
+    </div>
+</x-panel>
---
+++ resources/views/components/job-card.blade.php
@@ -0,0 +1,20 @@
+<x-panel class="flex flex-col text-center">
+    <div class="self-start text-sm">Laracasts</div>
+
+    <div class="py-8">
+        <h3 class="group-hover:text-blue-600 text-xl font-bold transition-colors duration-300">Video Producer</h3>
+        <p class="text-sm mt-4">Full Time - From 60,000 USD</p>
+    </div>
+
+    <div class="flex justify-between items-center mt-auto">
+        <div>
+            <x-tag size="small">asd</x-tag>
+            <x-tag size="small">gfdg</x-tag>
+            <x-tag size="small">dweetg</x-tag>
+
+        </div>
+
+        <x-employer-logo :width="42"></x-employer-logo>
+
+    </div>
+</x-panel>
